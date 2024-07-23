cabaa555e080f4cb9fce827e966d608801cd2956
DragosGhinea <dragos.dev12@gmail.com>
2024-07-23 09:10:30 +0300

    Adding variables for the MenuItemStackCfg

--- abstraction/src/main/java/com/dragosghinea/royale/menus/item/MenuItemStackCfg.java
+++ abstraction/src/main/java/com/dragosghinea/royale/menus/item/MenuItemStackCfg.java
@@ -1,9 +1,21 @@
 package com.dragosghinea.royale.menus.item;
 
 import com.dragosghinea.royale.internal.utils.item.ItemStackCfg;
+import com.dragosghinea.royale.menus.item.click.action.ClickActionCfg;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.util.List;
+
+@Getter
+@Setter
 public class MenuItemStackCfg extends ItemStackCfg {
 
-
-    
+    @JsonProperty("click-actions")
+    private List<ClickActionCfg> clickActions;
+    @JsonProperty("slot")
+    private Integer slot;
+    @JsonProperty("slots")
+    private List<Integer> slots;
 }
